f6838944e6db2cee11b820fce822bcaf198091f1
yym0807 <kevinyeh05@gmail.com>
2023-12-20 11:20:02 +0800

    change window size and board color
    原本的視窗太大了我的筆電塞不下

--- 圖形介面架構.cpp
+++ 圖形介面架構.cpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <cmath>
 
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 990;
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 750;
 
 //Starts up SDL and creates window
 bool init();
@@ -151,8 +151,8 @@ int main( int argc, char* args[] )
 			SDL_Event e;
 
 			//While application is running
-			double ori_x = 300, ori_y = 180;
-			double bo_w = 800, bo_h = 720;
+			double bo_w = SCREEN_HEIGHT / 10 * 8, bo_h = SCREEN_HEIGHT / 10 * 8;
+			double ori_x = (SCREEN_WIDTH - bo_w) / 2, ori_y = (SCREEN_HEIGHT - bo_h) / 2;
 			double gr_w = bo_w / 8, gr_h = bo_h / 8;
 			while( !quit )
 			{
@@ -171,22 +171,34 @@ int main( int argc, char* args[] )
 				SDL_RenderClear( gRenderer );
 				
 				//Render yellow filled quad
-				SDL_Rect fillRect = { ori_x, ori_y, bo_w, bo_h };
-				SDL_SetRenderDrawColor( gRenderer, 255, 255, 224, 0xFF );		
-				SDL_RenderFillRect( gRenderer, &fillRect );
+//				SDL_Rect fillRect = { ori_x, ori_y, bo_w, bo_h };
+//				SDL_SetRenderDrawColor( gRenderer, 255, 255, 224, 0xFF );		
+//				SDL_RenderFillRect( gRenderer, &fillRect );
+				for(int i = 0; i < 8; i++){
+					for(int j = 0; j < 8; j++){
+						if(i + j & 1){
+							SDL_Rect fillRect = { ori_x + i * gr_w, ori_y + j * gr_h, gr_w, gr_h };
+							SDL_SetRenderDrawColor( gRenderer, 0xB0, 0xD0, 0xEE, 0xFF );		
+							SDL_RenderFillRect( gRenderer, &fillRect );
+						}
+					}
+				}
 				
 				//Draw black horizontal lines
 				SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
 				for(int i = 0; i < 9; i++){
 					SDL_RenderDrawLine( gRenderer, ori_x, ori_y + i * gr_h, ori_x + bo_w, ori_y + i * gr_h );	
 				}		
-				
+//				SDL_RenderDrawLine( gRenderer, ori_x, ori_y, ori_x + bo_w, ori_y);
+//				SDL_RenderDrawLine( gRenderer, ori_x, ori_y + bo_h, ori_x + bo_w, ori_y + bo_h);
 				//Draw black vertical lines
 				SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
 				for(int i = 0; i < 9; i++){
 					SDL_RenderDrawLine( gRenderer, ori_x + i * gr_w, ori_y, ori_x + i * gr_w, ori_y + bo_h);	
 				}	
-
+//				SDL_RenderDrawLine( gRenderer, ori_x, ori_y, ori_x, ori_y + bo_h);
+//				SDL_RenderDrawLine( gRenderer, ori_x + bo_w, ori_y, ori_x + bo_w, ori_y + bo_h);
+				
 				//Update screen
 				SDL_RenderPresent( gRenderer );
 			}
